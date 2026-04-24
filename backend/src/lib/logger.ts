b8bbde05b0783aecef927c4f9af023eaa98c8e43
Toshiaki Wakabayashi <genzouw@gmail.com>
2026-04-24 09:36:13 +0900

    fix: 🐛 [Code Review] loggerの引数型をany[]からunknown[]に変更し型安全性を向上
    eslint-disableコメントを削除し、引数型をunknown[]に変更することで
    型安全性を向上させました。console.info/warn/error/debugはunknown[]を
    受け付けるため、機能的な変更はありません。
    
    レビューコメント: https://github.com/genzouw/toique/pull/121#discussion_r3134726349
    レビュアー: gemini-code-assist
    優先度: low

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export const logger = {
-  info: (...args: any[]) => console.info(...args),
-  warn: (...args: any[]) => console.warn(...args),
-  error: (...args: any[]) => console.error(...args),
-  debug: (...args: any[]) => console.debug(...args),
+  info: (...args: unknown[]) => console.info(...args),
+  warn: (...args: unknown[]) => console.warn(...args),
+  error: (...args: unknown[]) => console.error(...args),
+  debug: (...args: unknown[]) => console.debug(...args),
 };
